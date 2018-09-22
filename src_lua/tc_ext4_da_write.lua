2c36520a0963f9c580d8e0206f7bfa3c6753a306
Patrick Fay <pf5151@gmail.com>
2018-09-22 15:14:46 -0400

    add lua files/routines for derived events trace-cmd based on ext4:ext4_da_write and ext4:ext4_direct_IO events

---
+++ src_lua/tc_ext4_da_write.lua
@@ -0,0 +1,116 @@
+-- Copyright (c) 2018 Patrick Fay
+--
+-- License http://opensource.org/licenses/mit-license.php MIT License
+
+function got_problems( error_msg )
+	return "got_problems handler: " .. error_msg
+end
+
+printf = function(s,...)
+           return io.write(s:format(...))
+end -- function
+
+function mysplit(inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t= {} ; i=1
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                t[i] = str
+                i = i + 1
+        end
+        return t
+end
+
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+
+function ext4_da_write(verbose)
+	print "in lua routine ext4_da_write"
+	local ts0
+
+	if type(new_cols_hash) ~= "table" then
+		new_cols_hash = {}
+		for k,t in ipairs(new_cols) do
+    		new_cols_hash[t] = k
+		end
+		local tst = {"MiB/s", "duration", "bytes", "area"}
+		for k,t in ipairs(tst) do
+			local idx = new_cols_hash[t]
+			if idx == nil then
+				error("expected to find field '"..t.."' in new_cols list")
+			end
+		end
+	end
+	--for k,t in ipairs(data_cols) do
+    --    printf("data_cols[%s]= %s\n", k, data_cols[k])
+	--end
+	--for k,t in ipairs(data_vals) do
+    --    printf("data_vals[%s]= %s\n", k, data_vals[k])
+	--end
+	if type(tbl) ~= "table" then
+		tbl = {}
+	end
+	local cur_state = -1
+	local evt = data_vals[1]
+	local ts  = tonumber(data_vals[2])
+	local ky = data_vals[3]
+	local bytes = 0
+	--local action = "unk"
+	if evt == "ext4_da_write_begin" then 
+		local b, e = string.find(ky, " flags ")
+		if b == nil then
+			error("got problem with ext4_da_write_begin str= "..ky)
+		end
+		ky = string.sub(ky, 1, b)
+		b, e = string.find(ky, " len ")
+		bytes = tonumber(string.sub(ky, e))
+		printf("ky= '%s', bytes= %d\n", ky, bytes);
+	else
+		local b, e = string.find(ky, " copied ") -- reads... nah
+		if b == nil then
+			b, e = string.find(ky, " flags ") -- writes ... nah
+			if b == nil then
+				error("got problem with ext4_da_write_end str= "..ky)
+			end
+			--action = "wr"
+		else
+			--action = "rd"
+		end
+		ky = string.sub(ky, 1, b)
+	end
+--  wait.x-17709 [000] 15154023630678: ext4_da_write_begin:  dev 8,6 ino 287900 pos 0 len 6 flags 0
+--  wait.x-17709 [000] 15154023637835: ext4_da_write_end:    dev 8,6 ino 287900 pos 0 len 6 copied 6
+	if tbl[ky] == nil then
+		tbl[ky] = {ts, bytes}
+	else
+		local new_val = ts - tbl[ky][1]
+		printf("ext4_da_write in lua= %d\n", new_val)
+		local MiBs_idx = new_cols_hash["MiB/s"];
+		local dura_idx = new_cols_hash["duration"];
+		local bytes_idx = new_cols_hash["bytes"];
+		local area_idx = new_cols_hash["area"];
+		-- below assumes ts is in ns
+		local ns_factor = 1.0e-9
+		new_vals[dura_idx]   = ns_factor * new_val
+		new_vals[bytes_idx]  = tbl[ky][2]
+		new_vals[area_idx] = "system " -- ..action
+		if new_val > 0.0 then
+			new_vals[MiBs_idx] = tbl[ky][2] / (ns_factor * new_val)
+		else
+			new_vals[MiBs_idx] = 0.0;
+		end
+		tbl[ky] = nil
+	end
+end
---
+++ src_lua/tc_ext4_direct_IO.lua
@@ -0,0 +1,159 @@
+-- Copyright (c) 2018 Patrick Fay
+--
+-- License http://opensource.org/licenses/mit-license.php MIT License
+
+function got_problems( error_msg )
+	return "got_problems handler: " .. error_msg
+end
+
+printf = function(s,...)
+           return io.write(s:format(...))
+end -- function
+
+function mysplit(inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t= {} ; i=1
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                t[i] = str
+                i = i + 1
+        end
+        return t
+end
+
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+
+function ext4_direct_IO(verbose)
+	print "in lua routine ext4_direct_IO"
+	local ts0
+
+	if type(new_cols_hash) ~= "table" then
+		new_cols_hash = {}
+		for k,t in ipairs(new_cols) do
+    		new_cols_hash[t] = k
+		end
+		data_cols_hash = {}
+		for k,t in ipairs(data_cols) do
+    		data_cols_hash[t] = k
+		end
+		local MiBs_idx = new_cols_hash["MiB/s"];
+		if MiBs_idx == nil then
+			error("expected to find field 'MiB/s' in new_cols list")
+		end
+		local dura_idx = new_cols_hash["duration"];
+		if dura_idx == nil then
+			error("expected to find field 'duration' in new_cols list")
+		end
+		local bytes_idx = new_cols_hash["bytes"];
+		if bytes_idx == nil then
+			error("expected to find field 'bytes' in new_cols list")
+		end
+		local area_idx = new_cols_hash["area"];
+		if area_idx == nil then
+			error("expected to find field 'area' in new_cols list")
+		end
+		local idx = data_cols_hash["ts"];
+		if idx == nil then
+			error("expected to find field 'ts' in data_cols list")
+		end
+		idx = data_cols_hash["event"];
+		if idx == nil then
+			error("expected to find field 'event' in data_cols list")
+		end
+		idx = data_cols_hash["extra_str"];
+		if idx == nil then
+			error("expected to find field 'extra' in data_cols list")
+		end
+		local comm_idx = data_cols_hash["comm"];
+		if comm_idx == nil then
+			error("expected to find field 'comm' in data_cols list")
+		end
+		local pid_idx = data_cols_hash["pid"];
+		if pid_idx == nil then
+			error("expected to find field 'pid' in data_cols list")
+		end
+		local tid_idx = data_cols_hash["tid"];
+		if tid_idx == nil then
+			error("expected to find field 'tid' in data_cols list")
+		end
+	end
+	--for k,t in ipairs(data_cols) do
+    --    printf("data_cols[%s]= %s\n", k, data_cols[k])
+	--end
+	--for k,t in ipairs(data_vals) do
+    --    printf("data_vals[%s]= %s\n", k, data_vals[k])
+	--end
+	if type(tbl) ~= "table" then
+		tbl = {}
+	end
+	local cur_state = -1
+	local evt = data_vals[data_cols_hash["event"]]
+	local ts  = tonumber(data_vals[data_cols_hash["ts"]])
+	local ky = data_vals[data_cols_hash["extra_str"]]
+	local bytes = 0
+	local b, e = string.find(ky, " rw ")
+	if b == nil then
+		error("got problem with ext4_direct_IO_enter/exit str= "..ky.." ts= "..ts)
+	end
+	local rdwr = string.sub(ky, e+1, e+1)
+	printf("in lua rdwr= %s\n", rdwr)
+	local action
+	if rdwr == "0" then
+		action = "rd"
+	else
+		action = "wr"
+	end
+	ky = string.sub(ky, 1, b)
+	b, e = string.find(ky, " len ")
+	if b == nil then
+		error("got problem with ext4_direct_IO_enter/exit str= "..ky.." ts= "..ts)
+	end
+	bytes = tonumber(string.sub(ky, e))
+	--if evt == "ext4_direct_IO_enter" then 
+	--	printf("ky= '%s', bytes= %d\n", ky, bytes);
+	--end
+--	spin.x-17593 [001] 15110116275907: ext4_direct_IO_enter: dev 8,6 ino 302391 pos 104792064 len 65536 rw 1
+--	spin.x-17593 [001] 15110116443521: ext4_direct_IO_exit:  dev 8,6 ino 302391 pos 104792064 len 65536 rw 1 ret 65536
+
+	if tbl[ky] == nil then
+		tbl[ky] = {ts, bytes}
+	else
+		local new_val = ts - tbl[ky][1]
+		local MiBs_idx = new_cols_hash["MiB/s"];
+		local dura_idx  = new_cols_hash["duration"];
+		local bytes_idx = new_cols_hash["bytes"];
+		local area_idx  = new_cols_hash["area"];
+		local comm_idx  = data_cols_hash["comm"];
+		local pid_idx   = data_cols_hash["pid"];
+		local tid_idx   = data_cols_hash["tid"];
+		local str = data_vals[comm_idx] .. " " ..  data_vals[pid_idx] .. " " .. data_vals[tid_idx] .. " " .. action
+		-- below assumes ts is in ns
+		local ns_factor = 1.0e-9
+		new_vals[dura_idx]   = ns_factor * new_val
+		new_vals[bytes_idx]  = tbl[ky][2]
+
+		--new_vals[area_idx]   = "system"
+		new_vals[area_idx]   = str
+		if new_val > 0.0 then
+			-- below assumes ts is in ns
+			new_vals[MiBs_idx] = tbl[ky][2] / (ns_factor * new_val)
+		else
+			new_vals[MiBs_idx] = 0.0;
+		end
+		--printf("ext4_direct_IO in lua= %d, MB/s= %f, str= %s, rdwr= %s\n", new_val, new_vals[MiBs_idx], str, rdwr)
+		tbl[ky] = nil
+	end
+end
