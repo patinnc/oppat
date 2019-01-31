45e3f1e3bf63fb9ad9de28e8e608bc016ca56166
Patrick Fay <pf5151@gmail.com>
2019-01-31 12:55:53 -0500

    change order of compile

--- mk_spin.bat
+++ mk_spin.bat
@@ -1,12 +1,12 @@
 
-cl /EHsc /Ox /Zi -Iinc src/win_msr.c spin_wait/wait.cpp src/utils.cpp  /Fe:bin/wait.exe
-if %ERRORLEVEL% GTR 0 goto err
-del win_msr.obj wait.obj utils.obj
-
 
 cl /EHsc /Ox /Zi -Iinc spin_wait/spin.cpp src/utils.cpp spin_wait/trace_marker.cpp /Fe:bin/spin.exe
 if %ERRORLEVEL% GTR 0 goto err
 del spin.obj utils.obj
+
+cl /EHsc /Ox /Zi -Iinc src/win_msr.c spin_wait/wait.cpp src/utils.cpp  /Fe:bin/wait.exe
+if %ERRORLEVEL% GTR 0 goto err
+del win_msr.obj wait.obj utils.obj
 
 cl /EHsc spin_wait/win_send_signal.cpp /Fe:bin/win_send_signal.exe
 if %ERRORLEVEL% GTR 0 goto err
