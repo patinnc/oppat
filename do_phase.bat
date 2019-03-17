c7f7f2f73a1cc1523965ffb5a8043763dc7d9585
Patrick Fay <pf5151@gmail.com>
2019-03-17 14:10:53 -0400

    change to gen geekbench pngs

--- do_phase.bat
+++ do_phase.bat
@@ -2,8 +2,8 @@ set ph_end=Single-Core PDF Rendering
 set ph_end=Single-Core LZMA
 set ph_end=Multi-Core Memory Bandwidth
 del pat*.png
-make && bin\oppat.exe -r ..\oppat_data\lnx\multi7 --cpu_diagram web\haswell_block_diagram.svg --ph_image pat --ph_step_int 0.1 --phase0 "MT L3 RD BW" --phase1 "end phase MT L2 RD BW test"  > tmp.jnk
-goto :EOF
 make && bin\oppat.exe -r ..\oppat_data\lnx\gb8 --cpu_diagram web\haswell_block_diagram.svg --phase0 "Single-Core AES" --phase1 "%ph_end%" --ph_image pat --ph_step_int 0.1 > tmp.jnk
+goto :EOF
+make && bin\oppat.exe -r ..\oppat_data\lnx\multi7 --cpu_diagram web\haswell_block_diagram.svg --ph_image pat --ph_step_int 0.1 --phase0 "begin phase MT mem RD BW test" --phase1 "end phase MT spin test"  > tmp.jnk
 make && bin\oppat.exe -r ..\oppat_data\lnx\gb8 --cpu_diagram web\haswell_block_diagram.svg --phase AES --ph_image pat --ph_step_int 0.1,30 > tmp.jnk
 
