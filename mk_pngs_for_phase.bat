63a26774fed6a45fd23f5b6808fa97e5420ac4f7
Patrick Fay <pf5151@gmail.com>
2019-04-20 23:52:13 -0400

    Add bat file to make PNGs for 'per phase'
    Here is an example file for how to create 1 PNG for each phase
    in the file.
    The bat file will create files named 'pat0*.png' and pat_base.png.
    The pat_base.png is the block diagram without any stats.
    The pat00000.png is the 1st phase, pat000*1.png is the next phase,
    etc.

---
+++ mk_pngs_for_phase.bat
@@ -0,0 +1,2 @@
+
+make & bin\oppat.exe -r ..\oppat_data\lnx\arm_multi9 --cpu_diagram web\arm_cortex_a53_block_diagram.svg --phase0 "end phase MT mem RD BW test" --phase1 "end phase MT spin test" --by_phase --ph_image pat --img_wxh_pxls 1148,1326 > tmpa.jnk
