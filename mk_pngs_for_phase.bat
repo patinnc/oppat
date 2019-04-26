5cf017e62a7178d261b6c8a7082c9a37732b6d62
Patrick Fay <pf5151@gmail.com>
2019-04-26 10:49:28 -0400

    Add arm geekbench cmd line

--- mk_pngs_for_phase.bat
+++ mk_pngs_for_phase.bat
@@ -1,2 +1,5 @@
 
+make & bin\oppat.exe -r ..\oppat_data\lnx\arm_gb9 --cpu_diagram web\arm_cortex_a53_block_diagram.svg --phase0 "both Blowfish" --phase1 "Stream Triad" --by_phase --ph_image pat --img_wxh_pxls 1148,1326 > tmpa.jnk
+goto :EOF
 make & bin\oppat.exe -r ..\oppat_data\lnx\arm_multi9 --cpu_diagram web\arm_cortex_a53_block_diagram.svg --phase0 "end phase MT mem RD BW test" --phase1 "end phase MT spin test" --by_phase --ph_image pat --img_wxh_pxls 1148,1326 > tmpa.jnk
+
