39584c1cd9e684fddc06ccc080658bfb3d5a2c00
Patrick Fay <patrick.99.fay@gmail.com>
2019-04-26 11:25:40 -0400

    Add gcc -mtune option
    Add mtune option for building on arm cortex a53

--- mk_spin.sh
+++ mk_spin.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-g++ -std=c++11 -Iinc -O4 -g spin_wait/clocks.cpp -o bin/clocks.x -lpthread
-g++ -std=c++11 -Iinc -O4 -g spin_wait/spin.cpp src/utils.cpp spin_wait/trace_marker.cpp -o bin/spin.x -lpthread
+MTUNE=" -mtune=cortex-a53 "
+
+g++ $MTUNE -std=c++11 -Iinc -O4 -g spin_wait/clocks.cpp -o bin/clocks.x -lpthread
+g++ $MTUNE -std=c++11 -Iinc -O4 -g spin_wait/spin.cpp src/utils.cpp spin_wait/trace_marker.cpp -o bin/spin.x -lpthread
 ls -l bin/spin.x
 
-g++ -std=c++11 -Iinc -O3 -g spin_wait/wait.cpp src/utils.cpp -o bin/wait.x
+g++ $MTUNE -std=c++11 -Iinc -O3 -g spin_wait/wait.cpp src/utils.cpp -o bin/wait.x
 ls -l bin/wait.x
 
 
