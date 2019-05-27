31fdc6c6d0b1ddf420775ea61e1946c09ab2088a
Patrick Fay <pf5151@gmail.com>
2019-05-26 23:02:55 -0400

    Add --follow_proc and new directories

--- mk_pngs_for_phase.bat
+++ mk_pngs_for_phase.bat
@@ -1,5 +1,6 @@
+del pat0*.png
+make & bin\oppat.exe -r ..\oppat_data\lnx\arm_gb10 --cpu_diagram web\arm_cortex_a53_block_diagram.svg --phase0 "both Blowfish" --phase1 "Stream Triad" --by_phase --ph_image pat --img_wxh_pxls 1148,1326 --follow_proc geekbench > tmpa.jnk
 
-make & bin\oppat.exe -r ..\oppat_data\lnx\arm_gb9 --cpu_diagram web\arm_cortex_a53_block_diagram.svg --phase0 "both Blowfish" --phase1 "Stream Triad" --by_phase --ph_image pat --img_wxh_pxls 1148,1326 > tmpa.jnk
 goto :EOF
-make & bin\oppat.exe -r ..\oppat_data\lnx\arm_multi9 --cpu_diagram web\arm_cortex_a53_block_diagram.svg --phase0 "end phase MT mem RD BW test" --phase1 "end phase MT spin test" --by_phase --ph_image pat --img_wxh_pxls 1148,1326 > tmpa.jnk
+make & bin\oppat.exe -r ..\oppat_data\lnx\arm_multi10 --cpu_diagram web\arm_cortex_a53_block_diagram.svg --phase0 "end phase MT mem RD BW test" --phase1 "end phase MT spin test" --by_phase --ph_image pat --img_wxh_pxls 1148,1326 --follow_proc spin.x > tmpa.jnk
 
