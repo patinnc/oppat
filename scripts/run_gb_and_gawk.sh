645601a0facda3dcfb4e211fbeea76fad981c949
Patrick Fay <pf5151@gmail.com>
2019-06-01 17:30:49 -0400

    Pass rd_md=0 to gawk script
    scripts\gb_rd_output.gawk was changed to to allow creating
    gb_phase.tsv after the run is complete.
    Add the '-v rd_md="0"' args to the script to indicate we want to
    create the gb_phase.tsv during the run (that is, while
    run_perf_gb_haswell.sh is running).

--- scripts/run_gb_and_gawk.sh
+++ scripts/run_gb_and_gawk.sh
@@ -3,5 +3,5 @@
 gb_bin=$1
 gwk_scr=$2
 odir=$3
-$gb_bin | gawk -v odir="$odir" -f $gwk_scr
+$gb_bin | gawk -v rd_md="0" -v odir="$odir" -f $gwk_scr
 #~/geekbench/Geekbench-4.3.3-Linux/geekbench4 |gawk -f gb_rd_output.gawk
