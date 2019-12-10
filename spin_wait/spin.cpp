c52307bf57e854ad1bd97801e69c9170c527130f
Patrick Fay <pf5151@gmail.com>
2019-12-09 19:57:44 -0800

    add line indicating which cpu/node does what in mem_bw_remote test

--- spin_wait/spin.cpp
+++ spin_wait/spin.cpp
@@ -635,6 +635,8 @@ float mem_bw(unsigned int i)
 			cpu_to_switch_to = nodes_cpulist[other_nd][cpu_order_for_nd];
 		}
 		if (cpu_to_switch_to != -1) {
+			printf("thread[%.3d] malloc'd memory on cpu= %d node= %d and going to read the memory on cpu= %d node= %d\n", 
+				i, i, nd, cpu_to_switch_to, other_nd);
 			pin(cpu_to_switch_to);
 			my_msleep(0);
 		} else {
