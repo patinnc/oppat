8a8162101bd920b5733d538a4fb984f555b01e73
Patrick Fay <pfay1@ext.uber.com>
2020-01-18 23:02:15 -0800

    add getopts routine to spin compile scripts

--- mk_spin.sh
+++ mk_spin.sh
@@ -12,9 +12,18 @@ if [ "$OSTYPE" == "Darwin" ]; then
  STATIC2=
  STATIC3=
 fi
+OBJ=obj
+OBJEXT=o
+if [ ! -d bin ]; then
+  mkdir bin
+fi
+if [ ! -d $OBJ ]; then
+  mkdir $OBJ
+fi
 echo "OS= $OSTYPE"
+gcc $MTUNE  -Iinc -O2 -c src/mygetopt.c -o $OBJ/mygetopt.$OBJEXT
 g++ $MTUNE -std=c++11 -Iinc -O4 -g spin_wait/clocks.cpp src/utils.cpp -o bin/clocks.x -lpthread
-g++ $MTUNE -std=c++11 $STATIC1  -Iinc -O4 -g spin_wait/spin.cpp src/utils.cpp src/utils2.cpp spin_wait/trace_marker.cpp -o bin/spin.x -pthread $STATIC2 -lpthread $STATIC3
+g++ $MTUNE -std=c++11 $STATIC1  -Iinc -O4 -g spin_wait/spin.cpp $OBJ/mygetopt.$OBJEXT src/utils.cpp src/utils2.cpp spin_wait/trace_marker.cpp -o bin/spin.x -pthread $STATIC2 -lpthread $STATIC3
 ls -l bin/spin.x
 
 g++ $MTUNE -std=c++11 -Iinc -O3 -g spin_wait/wait.cpp src/utils.cpp -o bin/wait.x
