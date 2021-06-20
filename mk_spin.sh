5cac8cadf75ac1024cffd5b18b4b17f65f92705d
Patrick Fay <pfay1@ext.uber.com>
2021-06-19 23:52:29 -0400

    fix compiling on macbook

--- mk_spin.sh
+++ mk_spin.sh
@@ -7,10 +7,12 @@ OSTYPE=`uname`
 STATIC1=" -static "
 STATIC2=" -Wl,--whole-archive "
 STATIC3=" -Wl,--no-whole-archive "
+LIBNUMA=" -lnuma "
 if [ "$OSTYPE" == "Darwin" ]; then
  STATIC1=
  STATIC2=
  STATIC3=
+ LIBNUMA=
 fi
 OBJ=obj
 OBJEXT=o
@@ -23,7 +25,7 @@ fi
 echo "OS= $OSTYPE"
 gcc $MTUNE  -Iinc -O2 -c src/mygetopt.c -o $OBJ/mygetopt.$OBJEXT
 g++ $MTUNE -std=c++11 -Iinc -O4 -g spin_wait/clocks.cpp src/utils.cpp -o bin/clocks.x -lpthread
-g++ $MTUNE -std=c++11 $STATIC1 -Iinc -O4 -g spin_wait/spin.cpp $OBJ/mygetopt.$OBJEXT src/utils.cpp src/utils2.cpp spin_wait/trace_marker.cpp -o bin/spin.x -pthread $STATIC2 -lnuma -lpthread $STATIC3
+g++ $MTUNE -std=c++11 $STATIC1 -Iinc -O4 -g spin_wait/spin.cpp $OBJ/mygetopt.$OBJEXT src/utils.cpp src/utils2.cpp spin_wait/trace_marker.cpp -o bin/spin.x -pthread $STATIC2 $LIBNUMA -lpthread $STATIC3
 ls -l bin/spin.x
 
 g++ $MTUNE -std=c++11 -Iinc -O3 -g spin_wait/wait.cpp src/utils.cpp -o bin/wait.x
